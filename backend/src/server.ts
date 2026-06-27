213dbff8de34a1f2036bab83e4970a306b657a71
Divyanshu Raj <divyanshuraj23102005@gmail.com>
2026-06-27 18:34:21 +0530

    feat: configure database connection with mongoose and dotenv

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,22 @@
 const express = require('express')
+const connectDB = require('./config/db.ts');
+const dotenv = require('dotenv');
 
 const app = express();
+dotenv.config()
 
 app.use('/', async (req : any, res : any)=>{
     res.send("Hello World")
 })
 
-app.listen('7777', ()=>{
+
+connectDB()
+.then(()=>{
+    console.log("Connected to database !!");
+    app.listen('7777', ()=>{
     console.log("Listening on PORT 7777");
+    })
+})
+.catch((err:any)=>{
+    throw new Error(err)
 })
