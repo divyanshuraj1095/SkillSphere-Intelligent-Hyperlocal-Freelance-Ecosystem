18c97e48f077d85f98b7dad3e8802794992bf397
Divyanshu Raj <divyanshuraj23102005@gmail.com>
2026-06-27 19:30:37 +0530

    feat: add JWT authentication for user login and logout

--- backend/src/routes/authRoute.ts
+++ backend/src/routes/authRoute.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 const authRouter = express.Router();
 const Freelancer = require('./models/freelancer.ts')
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 authRouter.post('/signup', async(req : Request, res : Response)=>{
     try{
@@ -29,4 +30,41 @@ authRouter.post('/signup', async(req : Request, res : Response)=>{
                 message : "Error: "+err
             });
         }
-})
+});
+
+authRouter.post('/login', async(req : Request, res : Response)=>{
+    try{
+        const {email, password} = req.body;
+        if(!email || !password){
+           throw new Error("All fields are required!");
+        }
+        const user = await Freelancer.findOne({email : email});
+        if(!user){
+           throw new Error("User not found");
+        }
+        const valid = bcrypt.compare(password)
+        if(!valid){
+           throw new Error('Invalid credentials');
+        }
+
+        const token = jwt.sign({email}, process.env.JWT_TOKEN as string, {expiresIn : "5d"});
+
+        res.cookie("token", token);
+
+        res.json({
+            message : "Login Successful"
+        })
+        }
+        catch(err){
+            res.status(400).json({
+                message : "Error: "+err
+            });
+        }
+});
+
+authRouter.post('/logout', async(req : Request, res : Response)=>{
+    res.clearCookie("token")
+    res.json({
+        message : "Logout Successful"
+    })
+});
