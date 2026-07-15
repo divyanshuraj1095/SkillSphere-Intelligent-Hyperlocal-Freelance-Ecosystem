0f83daf67ccd8a38852780fa664807e38110f8fe
Divyanshu Raj <divyanshuraj23102005@gmail.com>
2026-07-15 16:25:52 +0530

    feat: implement notification routes; add CRUD operations for notifications with error handling

---
+++ backend/src/routes/notificationRoute.ts
@@ -0,0 +1,89 @@
+import express, {Request, Response} from "express"
+const notificationRouter = express.Router();
+import Notification from "../models/Notification";
+
+notificationRouter.get("/notification", async(req: Request, res: Response)=>{
+    try{
+        const notification = await Notification.find();
+        if(!notification){
+            throw new Error("No notification found!");
+        }
+
+        res.status(200).json({
+            message: "Notifications",
+            data: notification
+        })
+    }
+    catch(err){
+        res.status(400).json({
+            messsage: "Error: "+err
+        });
+    }
+});
+
+notificationRouter.get("/notification/:id", async(req: Request, res: Response)=>{
+    try{
+        const {id} = req.params;
+        const notification = await Notification.findById(id);
+        if(!notification){
+            throw new Error("No notification available");
+        }
+
+        res.status(200).json({
+            message: "Notification available",
+            data: notification
+        });
+    }
+    catch(err){
+        res.status(400).json({
+            message: "Error: "+err
+        });
+    }
+});
+
+notificationRouter.delete("/notification/:id", async(req: Request, res: Response)=>{
+    try{
+        const {id} = req.params;
+
+        const notification = await Notification.findByIdAndDelete(id);
+
+        if(!notification){
+           throw new Error("No notification found to delete");
+        }
+
+        res.status(200).json({
+            message: "Deleted SuccessFully"
+        });
+    }
+    catch(err){
+        res.status(400).json({
+            message: "Error: "+err
+        })
+    }
+});
+
+notificationRouter.patch("/notification/:id", async(req: Request, res: Response)=>{
+    try{
+        const {id} = req.params;
+        
+        const notification = await Notification.findByIdAndUpdate(id,{
+            isRead: true
+        },{new: true});
+
+        if(!notification){
+            throw new Error("No notification found to update");
+        }
+
+        res.status(200).json({
+            message: "Updated Successfully",
+            data: notification
+        })
+    }
+    catch(err){
+        res.status(400).json({
+            message: "Error: "+err
+        })
+    }
+});
+
+export default notificationRouter;
